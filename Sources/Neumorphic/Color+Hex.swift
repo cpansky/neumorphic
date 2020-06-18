398f5a56496dea2afa9ed6b9b31e6fb5266a4efe
Cedric Pansky <cedric@duetdisplay.com>
2020-06-18 11:07:27 -0600

    Fixed a bug on macOS targets (AppKit) where UIColor was being used, not NSColor as appropriate.

--- Sources/Neumorphic/Color+Hex.swift
+++ Sources/Neumorphic/Color+Hex.swift
@@ -6,13 +6,19 @@
 
 import SwiftUI
 
+#if canImport(UIKit)
+typealias OSColor = UIColor
+#else
+typealias OSColor = NSColor
+#endif
+
 extension Color {
     public init(red: Int, green: Int, blue: Int, alpha: CGFloat) {
         assert(red >= 0 && red <= 255, "Invalid red component")
         assert(green >= 0 && green <= 255, "Invalid green component")
         assert(blue >= 0 && blue <= 255, "Invalid blue component")
 
-        self.init(UIColor(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha))
+        self.init(OSColor(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha))
     }
     
     public init(rgb: Int, alpha: CGFloat) {
